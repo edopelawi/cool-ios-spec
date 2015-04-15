dda1b516e8dd599f7c4f0e080b958943bdbf213b
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2015-04-15 19:16:47 +0700

    Add CoolSpec project

---
+++ CoolSpec/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoolSpec
+//
+//  Created by Ricardo Pramana Suranta on 4/15/15.
+//  Copyright (c) 2015 Ice House. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
